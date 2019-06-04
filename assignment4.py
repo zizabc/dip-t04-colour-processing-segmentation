ac19dd3941b168e50f90b0bd67d25c170db8a3fb
andremsouza <msouza.andre@hotmail.com>
2019-06-04 00:34:37 -0300

    Created function to generate datasets for k-means Started k-means function.

--- assignment4.py
+++ assignment4.py
@@ -4,55 +4,121 @@
 # Course Code: SCC0251
 # Semester: 2019/1
 # Assignment: 4 - Colour image processing and segmentation
-#%%
+# %%
 import numpy as np
 import random
 import imageio
-import matplotlib.pyplot as plt # ! Remove before submitting
+import matplotlib.pyplot as plt  # ! Remove before submitting
 
-#%%
+# %%
 # * Defining functions for assignment
 
-# * Performs k-means algorithm of an input image
-def k_means(img, pixel_mode, k, n):
-    img_ret = np.zeros(img.shape, dtype=np.float)
+
+def img_k_means(img, pixel_mode, k, n, S=None):
+    """ Performs k-means algorithm of an input image.
+
+    The result segmented image I is a m x n image,
+    with labels defined by I(x, y) in {1, ..., k},
+    that is, each pixel receives a label relative to it's cluster.
+    """
+    # Declaring return image
+    img_ret = np.zeros(img.shape, dtype=np.uint8)
+    # Selecting centroids (with S as the seed)
+    random.seed(S)
     ids = np.sort(random.sample(range(0, img_i.shape[0]*img_i.shape[1]), k))
+    # Generating dataset for k-means
+    dataset = dataset_gen_from_img(img, pixel_mode)
+
+    # Iterating
+    for i in range(n):
+        dists = [np.linalg.norm(dataset - dataset[i]) for i in ids]
+        # for x in range(img.shape[0]):
+        #     for y in range(img.shape[1]):
+        #         # Computing Euclidean distance to each centroid
+        #         dists = np.array([np.linalg.norm(dataset[x*img.shape[0]+y] \
+        #             - dataset[i]) for i in ids])
+        #         img_ret[x, y] = np.argmin(dists) + 1
+
     return img_ret
 
-# * Root mean squared error (RMSE) for comparison
+
 def rmse(img_i, img_r):
-    return np.sqrt((1/(img_i.shape[0] * img_i.shape[1])) * np.sum(np.square(img_i.astype(float) - img_r.astype(float))))
+    """ Root mean squared error (RMSE) for comparison."""
+    return np.sqrt((1/(img_i.shape[0] * img_i.shape[1]))
+                   * np.sum(np.square(img_i.astype(float)
+                                      - img_r.astype(float))))
 
-# * Computes the euclidean distance of two images
-def euclidean_distance(img1, img2):
-    if(len(img1.shape) == 3):
-        return np.sum(np.square(img1.astype(float)-img2.astype(float)))/img1.shape[2]
-    elif(len(img1.shape) == 2):
-        return np.sum(np.square(img1.astype(float)-img2.astype(float)))
-    else: raise TypeError("Invalid shape for img1")
+# x -> # np.repeat(np.arange(0, img_i.shape[0], dtype=int), img_i.shape[1]*img_i.shape[2]).shape
+# y -> # np.array(list(np.repeat(np.arange(0, img_i.shape[1], dtype=int), img_i.shape[2]))*img_i.shape[0]).shape
 
-# * Normalizes input image between min_value and max_value
+
 def normalize(img, min_value=0, max_value=255):
-    return min_value + \
-        (img-img.min())*(max_value-min_value) / (img.max() - img.min())
+    """ Normalizes input image between min_value and max_value."""
+    return min_value \
+        + (img-img.min())*(max_value-min_value) / (img.max() - img.min())
 
-#%%
+
+def dataset_gen_from_img(img, pixel_mode):
+    """ Generates dataset from image, according to the selected pixel_mode
+
+    Result can be used as an argument to k_means function
+    pixel_mode: 1 -> RGB; 2 -> RGBxy; 3 -> luminance; 4 -> luminance, x, y;
+    """
+    dataset = None
+    if(pixel_mode == 1):
+        # R,G,B
+        dataset = np.array([img[x, y, :]
+                            for x in range(img.shape[0])
+                            for y in range(img.shape[1])],
+                           dtype=np.double)
+    elif(pixel_mode == 2):
+        # R,G,B,x,y
+        dataset = np.array([
+            [img[x, y, 0], img[x, y, 1], img[x, y, 2], x, y]
+            for x in range(img.shape[0])
+            for y in range(img.shape[1])],
+            dtype=np.double)
+    elif(pixel_mode == 3):
+        # Luminance
+        dataset = np.array([0.299 * img[x, y, 0] + 0.587 * img[x, y, 1]
+                            + 0.114 * img[x, y, 2]
+                            for x in range(img.shape[0])
+                            for y in range(img.shape[1])],
+                           dtype=np.double)
+    elif(pixel_mode == 4):
+        # Luminance, x, y
+        dataset = np.array([[0.299 * img[x, y, 0] + 0.587 * img[x, y, 1]
+                             + 0.114 * img[x, y, 2], x, y]
+                            for x in range(img.shape[0])
+                            for y in range(img.shape[1])],
+                           dtype=np.double)
+    else:
+        raise ValueError("Invalid value for pixel_mode")
+    return dataset
+
+
+# %%
 # * Main function
 if __name__ == "__main__":
     # get user input and check validity
-    ipath, rpath = str(input()).strip(), str(input()).strip() # i = input image, r = reference image
-    img_i, img_r = imageio.imread(ipath), imageio.imread(rpath) # reading images
-    pixel_mode = int(input(), base=10) # 1 -> RGB; 2 -> RGBxy; 3 -> luminance; 4 -> luminance, x, y;
-    if(pixel_mode not in [1, 2, 3, 4]): raise ValueError("Invalid input value for pixel_mode. Should be in [1, 2, 3, 4]")
-    k = int(input(), base=10) # k = number of clusters
-    if(k <= 0): raise ValueError("k should be a positive integer value -> k > 0")
-    n = int(input(), base=10) # n = number of iterations
-    if(n <= 0): raise ValueError("n should be a positive integer value -> n > 0")
-    S = input() # User-defined seed
-    random.seed(S)
+    # i = input image, r = reference image
+    ipath, rpath = str(input()).strip(), str(input()).strip()
+    img_i, img_r = imageio.imread(
+        ipath), imageio.imread(rpath)  # reading images
+    # 1 -> RGB; 2 -> RGBxy; 3 -> luminance; 4 -> luminance, x, y;
+    pixel_mode = int(input(), base=10)
+    if(pixel_mode not in [1, 2, 3, 4]):
+        raise ValueError(
+            "Invalid input value for pixel_mode. Should be in [1, 2, 3, 4]")
+    k = int(input(), base=10)  # k = number of clusters
+    if(k <= 0):
+        raise ValueError("k should be a positive integer value -> k > 0")
+    n = int(input(), base=10)  # n = number of iterations
+    if(n <= 0):
+        raise ValueError("n should be a positive integer value -> n > 0")
+    S = input()  # User-defined seed
+    random.seed(S)  # Testing errors for S
 
     # Compute k-means and output
     # TODO: Uncomment next line to test k_means function
-    print('%.4f' % rmse(k_means(img_i, pixel_mode, k, n), img_r))
-
-#%%
+    print('%.4f' % rmse(img_k_means(img_i, pixel_mode, k, n, S), img_r))
